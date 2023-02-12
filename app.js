b2148101b935912aea617a92f95e8221bb0333de
Aqdas Zishan <97790386+AqdasZishan@users.noreply.github.com>
2023-02-12 19:33:06 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const likeButton = document.querySelector(".like-button");
-const likesCount = document.querySelector(".likes-count");
-
-let likes = 0;
-
-likeButton.addEventListener("click", function(){
-  likes++;
-  likesCount.textContent = likes;
-});
